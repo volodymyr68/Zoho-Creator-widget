77cc388fb8d3b0b4fdd98040abfb94fc116681e1
volodymyr <lif4xiks@gmail.com>
2024-07-29 10:45:18 +0300

    added backlog feature

--- app/api/zohoApi.js
+++ app/api/zohoApi.js
@@ -1,4 +1,5 @@
 import {initializeZohoConstants, SPRINT_CONFIG, TASK_CONFIG, ZOHO_CONSTANTS} from '../utils/zohoConstants.js';
+import {modalSuccess} from "../utils/zohoHelpers.js";
 
 export const getAllSprints = async () => {
     try {
@@ -25,13 +26,40 @@ export const getAllTasks = async () => {
 export const updateSprint = async (config) => {
     try {
         const response = await ZOHO.CREATOR.API.updateRecord(config);
+        modalSuccess(response);
     } catch (error) {
+        alert("Error updating contact developer");
         console.error("Error updating record:", error);
         throw error;
     }
 };
 
-export const updateData = (sprintRecords) => {
+export const setBacklogToTasks = async (config) => {
+    try {
+        const response = await ZOHO.CREATOR.API.updateRecord(config);
+        modalSuccess(response);
+    } catch (error) {
+        alert("Error setting backlog to tasks");
+        console.error("Error setting backlog to tasks:", error);
+        throw error;
+    }
+}
+
+export const updateData = async (sprintRecords) => {
+     try{
+        await sprintUpdate(sprintRecords);
+     }catch (error) {
+         console.error("Error updating data:", error);
+     }
+     try{
+        await updateTasks();
+     }catch (error) {
+         console.error("Error updating tasks:", error);
+     }
+
+};
+
+const sprintUpdate = async (sprintRecords) => {
     document.getElementById("submit").addEventListener('click', async () => {
         const sprintSelect = document.getElementById('sprintSelect');
         const sprintID = sprintSelect.value;
@@ -53,15 +81,40 @@ export const updateData = (sprintRecords) => {
         const formData = { data : {Tasks: allTasks} };
 
 
-        const UPDATE_CONFIG = {
+        const SPRINT_CONFIG = {
             appName: ZOHO_CONSTANTS.APP_LINK_NAME || "",
             reportName: "All_Sprints",
             id: sprintID,
             data: formData
         };
 
-        await updateSprint(UPDATE_CONFIG);
-    });
-};
+        try{
+            await updateSprint(SPRINT_CONFIG);
+        }catch (error){
+            alert("Error updating sprint");
+            console.error("Error updating sprint:", error);
+        }
 
+    });
+}
+
+const updateTasks = async () => {
+    document.getElementById("backlog").addEventListener("click", () => {
+    let selectedTaskIds = [];
+    const checkboxes = document.querySelectorAll('.task-checkbox:checked');
+    checkboxes.forEach(checkbox => {
+        selectedTaskIds.push(checkbox.id);
+    });
+
+        selectedTaskIds.forEach(item=>{
+           const TASKS_CONFIG = {
+                 appName: ZOHO_CONSTANTS.APP_LINK_NAME || "",
+                 reportName: "All_Tasks",
+                 id: item,
+                 data : {data:{Status:"Backlog",Sprint:null}}
+             };
+             setBacklogToTasks(TASKS_CONFIG);
+        });
+    });
+}
 
--- app/utils/zohoHelpers.js
+++ app/utils/zohoHelpers.js
@@ -5,3 +5,10 @@ export const getQueryParams = async () => {
 export const getInitParams = async () => {
     return await ZOHO.CREATOR.UTIL.getInitParams();
 }
+
+export const modalSuccess = (response) => {
+    let model = document.getElementById("closeWidget");
+    model.classList.remove("hidden");
+    model.classList.add("active");
+    model.textContent  = `Success. ${response}. Please close widget`;
+}
